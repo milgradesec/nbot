a43fd72ff8e9c2034f4e8d493b0e06624b82aea8
milgradesec <38557680+milgradesec@users.noreply.github.com>
2021-06-07 17:32:25 +0200

    Add Kirt

--- internal/bot/minitas.go
+++ internal/bot/minitas.go
@@ -24,7 +24,7 @@ func (bot *Bot) minitaHandler(ctx *dgc.Ctx) {
 
 func (bot *Bot) addMinitaHandler(ctx *dgc.Ctx) {
 	msg := ctx.Event.Message
-	if msg.Author.Username != "MILGRADESEC" && msg.Author.Username != "L. L." {
+	if msg.Author.Username != "MILGRADESEC" && msg.Author.Username != "L. L." && msg.Author.Username != "Kirt" {
 		ctx.RespondText("Tu no tienes permiso para añadir nada") //nolint
 		return
 	}
